22261766ce965e34be16ede9995a07208b95aa05
Jesus Lozano <mjlozanoh98@gmail.com>
2021-02-25 17:43:01 -0500

    Integrated firebase into the application and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, Component } from 'react';
 import './App.css';
 import { Switch, Route } from "react-router-dom";
 
@@ -6,21 +6,44 @@ import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignOutPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
+import { auth } from './firebase/firebase.utils';
 
 
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: null
+    }
+  }
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route path="/shop" component={ShopPage}></Route>
-        <Route path="/signin" component={SignInAndSignOutPage}></Route>
-      </Switch>
-    </div>
-  );
+  //Auth management for the application
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+      console.log(user);
+    })
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/shop" component={ShopPage}></Route>
+          <Route path="/signin" component={SignInAndSignOutPage}></Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
+
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './header.styles.scss'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-
-const Header = () => {
+import { auth } from '../../firebase/firebase.utils';
+const Header = ({ currentUser }) => {
     return (
         <div className='header'>
             <Link className='logo-container' to='/'>
@@ -12,13 +12,17 @@ const Header = () => {
             <div className='options'>
                 <Link className='option' to='/shop'>
                     SHOP
-          </Link>
+                </Link>
                 <Link className='option' to='/shop'>
                     CONTACT
-          </Link>
-                <Link className='option' to='/signin'>
-                    SIGN IN
-          </Link>
+                </Link>
+
+                {
+                    currentUser ?
+                        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+                        :
+                        <Link className='option' to="/signin">SIGN IN</Link>
+                }
             </div>
         </div>
     );
