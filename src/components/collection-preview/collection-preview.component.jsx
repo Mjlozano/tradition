81590fb479667f79520260b3fed02dc8dd9eed63
ApoloJ <mjlozanoh98@gmail.com>
2021-05-19 09:09:20 -0500

    Cart State added to redux

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,9 +6,8 @@ const CollectionPreview = ({ title, items }) => {
         <div className="collection-preview">
             <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
-                {items.filter((item, idx) => idx < 4).map(({ id, ...otherItemProps }) => (
-
-                    <ColletionItem key={id} {...otherItemProps}></ColletionItem>
+                {items.filter((item, idx) => idx < 4).map((item) => (
+                    <ColletionItem key={item.id} item={item}></ColletionItem>
                 ))}
             </div>
         </div >
